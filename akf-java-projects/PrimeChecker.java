295ad5130430f842266fff6651e4526adcd85b3e
arshif <arshif@gmail.com>
2023-12-10 10:06:52 -0700

    First commit

---
+++ akf-java-projects/PrimeChecker.java
@@ -0,0 +1,24 @@
+public class PrimeChecker {
+    public static void main(String[] args) {
+        int number = 23; // Change this number to check for primality
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
